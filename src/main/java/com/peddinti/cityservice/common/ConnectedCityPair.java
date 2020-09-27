54896a3dc79fbba731ca11c010bf94f78ad151b3
Santosh Peddinti <34074944+santoshpeddinti@users.noreply.github.com>
2020-09-27 15:30:32 -0400

    readded

---
+++ src/main/java/com/peddinti/cityservice/common/ConnectedCityPair.java
@@ -0,0 +1,19 @@
+package com.peddinti.cityservice.common;
+
+public class ConnectedCityPair {
+	private String origin;
+	private String destination;
+	public String getOrigin() {
+		return origin;
+	}
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+
+}
